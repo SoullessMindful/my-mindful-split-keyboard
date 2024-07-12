10c87d9249dcd1ea1f775748ed339933068407f3
SoullessMindful <soullessmindful@gmail.com>
2024-07-12 13:04:09 +0200

    Setting up keyboard, inputs and outputs

--- my-mindful-split-keyboard.ino
+++ my-mindful-split-keyboard.ino
@@ -1,5 +1,26 @@
+#include <Keyboard.h>
+
+const byte OUTPUT_COUNT = 6;
+const byte OUTPUTS[OUTPUT_COUNT] = {A0, A1, A2, A3, A4, A5};
+
+const byte INPUT_COUNT = 8;
+const byte SPECIAL_SECTION_LEFT = 6;
+const byte SPECIAL_SECTION_RIGHT = 7;
+const byte INPUTS[INPUT_COUNT] = {
+  0, 1, 2, 4, 5, SPECIAL_SECTION_LEFT, SPECIAL_SECTION_RIGHT
+};
 
 void setup() {
+  for (byte i = 0; i < OUTPUT_COUNT; i++) {
+    pinMode(OUTPUTS[i], OUTPUT);
+    digitalWrite(OUTPUTS[i], HIGH);
+  }
+
+  for (byte j = 0; j < INPUT_COUNT; j++) {
+    pinMode(INPUTS[j], INPUT_PULLUP);
+  }
+
+  Keyboard.begin();
 }
 
 void loop() {
