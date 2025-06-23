624b82afe1bed950bea5ba41128c6966106221aa
SoullessMindful <soullessmindful@gmail.com>
2025-06-23 07:16:49 +0200

    Fixing input pins array

--- my-mindful-split-keyboard.ino
+++ my-mindful-split-keyboard.ino
@@ -5,44 +5,13 @@ const byte OUTPUTS[OUTPUT_COUNT] = {A0, A1, A2, A3, A4, A5};
 
 const byte INPUT_COUNT = 8;
 const byte INPUTS[INPUT_COUNT] = {
-  0, 1, 2, 4, 5, 6, 7
+  2, 3, 4, 5, 6, 7, 8, 9
 };
 
 const byte LAYER_COUNT = 3;
 const byte SYMBOL_LAYER = 2;
 const byte FUNCTION_LAYER = 1;
 const byte BASE_LAYER = 0;
-
-/*const char keys[LAYER_COUNT][INPUT_COUNT][OUTPUT_COUNT] = {
-  {
-    {KEY_TAB, KEY_ESC, KEY_CAPS_LOCK,
-    'q', 'a', 'z'},
-    {'w', 's', 'x',
-    'e', 'd', 'c'},
-    {'r', 'f', 'v',
-    't', 'g', 'b'},
-    {'y', 'h', 'n',
-    'u', 'j', 'm'},
-    {'i', 'k', ',',
-    'o', 'l', '.'},
-    {'p', ';', '/',
-    '\\', '\'', KEY_RIGHT_SHIFT},
-  },
-  {
-    {KEY_TAB, KEY_ESC, KEY_CAPS_LOCK,
-    KEY_F1, KEY_F13, KEY_INSERT},
-    {KEY_F2, KEY_F14, KEY_DELETE,
-    KEY_F3, KEY_F15, KEY_F18},
-    {KEY_F4, KEY_F16, KEY_F19,
-    KEY_F5, KEY_F17, KEY_F20},
-    {KEY_F6, KEY_LEFT_ARROW, KEY_HOME,
-    KEY_F7, KEY_DOWN_ARROW, KEY_PAGE_DOWN},
-    {KEY_F8, KEY_UP_ARROW, KEY_PAGE_UP,
-    KEY_F9, KEY_RIGHT_ARROW, KEY_END},
-    {KEY_F10, ';', '/',
-    KEY_F11, KEY_F12, KEY_PRINT_SCREEN},
-  },
-};*/
 
 unsigned long shiftLastTime;
 bool shiftLock = false;
